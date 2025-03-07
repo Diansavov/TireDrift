e1308ef6f46250773743d214d1ad70fa12353496
Diansavov <diansavov2@abv.bg>
2025-03-07 15:29:04 +0200

    RevertToArch

--- Program.cs
+++ Program.cs
@@ -12,7 +12,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<TiresDbContext>(options =>
-             options.UseSqlServer(builder.Configuration.GetConnectionString("TireDriftConnectionString")));
+             options.UseMySql(builder.Configuration.GetConnectionString("TireDriftConnectionString"), new MySqlServerVersion(new Version(10, 0, 1))));
+
 builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = false).AddRoles<IdentityRole>().AddEntityFrameworkStores<TiresDbContext>();
 
 builder.Services.Configure<IdentityOptions>(options =>
